330f716ec7650e72f4d5c6fe3db20a795acf0593
govinda <govinda@gmail.com>
2024-08-19 12:52:18 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ config({path: "./config/.env"})
 // }))
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.BACKEND_PART_URL],
+    origin: '*',
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true
 }));
