72b960e8097b6b2647c62d9973d6f7e01d2ea7bb
govinda <govinda@gmail.com>
2024-08-20 11:15:58 +0530

    change

--- Backend/middleware/errorMiddleware.js
+++ Backend/middleware/errorMiddleware.js
@@ -1,27 +1,23 @@
-export class errorhandler extends Error {
-  constructor(message, statusCode) {
-    super(message), (this.statusCode = statusCode);
-  }
-}
-
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
-  if (err.statusCode === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+  // Handle specific errors
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
     err = new errorhandler(message, 400);
   }
-  if (err.name == "JsonWebTokenError") {
-    const message = "Json web Token is invalid , Try Again";
+
+  if (err.name === "JsonWebTokenError") {
+    err = new errorhandler("Invalid token, try again", 401);
   }
+
   if (err.name === "TokenExpiredError") {
-    const message = "Json web Token is Expired , Try Again";
-    err = new errorhandler(message, 400);
+    err = new errorhandler("Token expired, try again", 401);
   }
-  if (err.name === "castError") {
-    const message = `Invalid${err.path}`;
-    err = new errorhandler(message, 400);
+
+  if (err.name === "CastError") {
+    err = new errorhandler(`Invalid ${err.path}`, 400);
   }
 
   const errorMessage = err.errors
@@ -29,6 +25,7 @@ export const errorMiddleware = (err, req, res, next) => {
         .map((err) => err.message)
         .join(" || ")
     : err.message;
+
   return res.status(err.statusCode).json({
     success: false,
     message: errorMessage,
